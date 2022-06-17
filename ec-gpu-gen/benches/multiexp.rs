9fb157531bc1de3d98f37e6ae764a737142e0a0f
Volker Mische <volker.mische@gmail.com>
2022-06-17 17:43:26 +0200

    fix: make multiexp benchmarks faster to run

--- ec-gpu-gen/benches/multiexp.rs
+++ ec-gpu-gen/benches/multiexp.rs
@@ -10,35 +10,36 @@ use group::{Curve, Group};
 use pairing::Engine;
 use rust_gpu_tools::Device;
 
-fn gpu_multiexp(num_elements: usize) {
+fn bench_multiexp(crit: &mut Criterion) {
+    let mut group = crit.benchmark_group("multiexp");
+    // The difference between runs is so little, hence a low sample size is OK.
+    group.sample_size(10);
+
     let devices = Device::all();
     let mut kern = MultiexpKernel::<Bls12>::create(&devices).expect("Cannot initialize kernel!");
     let pool = Worker::new();
 
     let mut rng = rand::thread_rng();
 
-    let bases = (0..num_elements)
-        .map(|_| <Bls12 as Engine>::G1::random(&mut rng).to_affine())
-        .collect::<Vec<_>>();
-
-    let exponents = (0..num_elements)
-        .map(|_| <Bls12 as Engine>::Fr::random(&mut rng).to_repr())
-        .collect::<Vec<_>>();
-
-    let (raw_bases, skip) = SourceBuilder::get((Arc::new(bases), 0));
-    kern.multiexp(&pool, raw_bases, Arc::new(exponents), skip)
-        .unwrap();
-}
-
-fn bench_multiexp(crit: &mut Criterion) {
-    let mut group = crit.benchmark_group("multiexp");
-    // The difference between runs is so little, hence a low sample size is OK.
-    group.sample_size(10);
     let num_elements = (10..20).map(|shift| 1 << shift).collect::<Vec<_>>();
     for num in num_elements {
         group.bench_with_input(BenchmarkId::from_parameter(num), &num, |bencher, &num| {
+            let bases_raw = (0..num)
+                .map(|_| <Bls12 as Engine>::G1::random(&mut rng).to_affine())
+                .collect::<Vec<_>>();
+
+            let exponents_raw = (0..num)
+                .map(|_| <Bls12 as Engine>::Fr::random(&mut rng).to_repr())
+                .collect::<Vec<_>>();
+            let exponents = Arc::new(exponents_raw);
+
+            let (bases, skip) = SourceBuilder::get((Arc::new(bases_raw), 0));
+
             bencher.iter(|| {
-                black_box(gpu_multiexp(num));
+                black_box(
+                    kern.multiexp(&pool, bases.clone(), exponents.clone(), skip)
+                        .unwrap(),
+                );
             })
         });
     }
